21636a110e8ee8d0d77b238af7efb7efa3b21fef
yogandrn <yogaandrianp21@gmail.com>
2021-12-12 19:28:27 +0700

    API Database

---
+++ Mobile/KoneksiAPI/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db   = "id11029_db_bumdes";
+
+$conn = mysqli_connect($host, $user, $password, $db) or die("Gagal menghubungi server!");;
+
+?>

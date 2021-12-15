18a1e00c427210047b125463b781a810a21f8c31
yogandrn <yogaandrianp21@gmail.com>
2021-12-15 10:19:44 +0700

    Bumdes Karya Mandiri
    Koneksi API

--- Mobile/KoneksiAPI/config.php
+++ Mobile/KoneksiAPI/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "root";
-$password = "";
-$db   = "id11029_db_bumdes";
+$user = "id17675329_yogandrn";
+$password = "PHPNative_100602";
+$db   = "id17675329_db_bumdes";
 
 $conn = mysqli_connect($host, $user, $password, $db) or die("Gagal menghubungi server!");;
 

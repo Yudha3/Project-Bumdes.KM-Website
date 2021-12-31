d3b9901860d44040e93920a83957d9ac0cb45aa7
yogandrn <75407016+yogandrn@users.noreply.github.com>
2021-12-31 14:58:49 +0700

    Update pindahData.php

--- Admin Bumdes v3/transaksi/pindahData.php
+++ Admin Bumdes v3/transaksi/pindahData.php
@@ -2,7 +2,19 @@
 include '../koneksi.php';
 
 $id = $_GET["id_transaksi"];
-$simpan = mysqli_query($koneksi, "INSERT INTO report_klr (id_transaksi,tgl_transaksi, total_transaksi,penerima,status) SELECT id_transaksi,tgl_transaksi,total_transaksi,penerima,status FROM transaksi WHERE id_transaksi ='$id'");
+
+$find= mysqli_query($koneksi, "SELECT * FROM transaksi WHERE id_transaksi = '$id'");
+while ($get = mysqli_fetch_assoc($find)) {
+    $id_trx = $get['id_transaksi'];
+    $tgl = substr($get['tgl_transaksi'], 0, -9);
+    $id_user = $get['id_user'];
+    $penerima = $get['penerima'];
+    $alamat = $get['alamat'];
+    $no_telp = $get['no_telp'];
+    $id_ongkir = $get['id_ongkir'];
+    $total = $get['total_transaksi'];
+}
+$simpan = mysqli_query($koneksi, "INSERT INTO report_klr (id_transaksi, tgl_transaksi, penerima, alamat, total_transaksi, status) VALUES ('$id_trx','$tgl','$penerima','$alamat','$total', 'Selesai') ");
 
 $delete = mysqli_query($koneksi, "DELETE FROM transaksi WHERE id_transaksi = '$id'");
 $delete = mysqli_query($koneksi, "DELETE FROM transaksi_produk WHERE id_transaksi = '$id'");

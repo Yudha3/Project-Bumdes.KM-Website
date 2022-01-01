4d392a435ed41c866caed6a6507b607222a958b2
yogandrn <yogaandrianp21@gmail.com>
2022-01-01 08:56:37 +0700

    Update pindahData.php

--- Admin Bumdes v3/transaksi/pindahData.php
+++ Admin Bumdes v3/transaksi/pindahData.php
@@ -2,26 +2,29 @@
 include '../koneksi.php';
 
 $id = $_GET["id_transaksi"];
+$find = mysqli_query($koneksi, "SELECT * FROM transaksi WHERE id_transaksi ='$id'");
 
-$find= mysqli_query($koneksi, "SELECT * FROM transaksi WHERE id_transaksi = '$id'");
-while ($get = mysqli_fetch_assoc($find)) {
-    $id_trx = $get['id_transaksi'];
-    $tgl = substr($get['tgl_transaksi'], 0, -9);
-    $id_user = $get['id_user'];
-    $penerima = $get['penerima'];
-    $alamat = $get['alamat'];
-    $no_telp = $get['no_telp'];
-    $id_ongkir = $get['id_ongkir'];
-    $total = $get['total_transaksi'];
-}
-$simpan = mysqli_query($koneksi, "INSERT INTO report_klr (id_transaksi, tgl_transaksi, penerima, alamat, total_transaksi, status) VALUES ('$id_trx','$tgl','$penerima','$alamat','$total', 'Selesai') ");
+while ($data = mysqli_fetch_assoc($find)){
+    $id_trx = ['id_transaksi'];
+    $tgl = substr($data['tgl_transaksi'], 0, -9);
+    $penerima = ['penerima'];
+    $alamat = ['alamat'];
+    $total = ['total_transaksi'];
 
-$delete = mysqli_query($koneksi, "DELETE FROM transaksi WHERE id_transaksi = '$id'");
-$delete = mysqli_query($koneksi, "DELETE FROM transaksi_produk WHERE id_transaksi = '$id'");
-if (!$delete) {
-        die("Gagal menghapus data: " . mysqli_errno($koneksi) .
-            " - " . mysqli_error($koneksi));
+    mysqli_query($koneksi, "INSERT INTO report_klr (id_transaksi, tgl_transaksi, penerima, alamat, total_transaksi,status) VALUES ('$id_trx','$tgl','$penerima','$alamat','$total','Selesai')");
+
+    if (mysqli_affected_rows($koneksi) > 0) {
+        $deleteTransaksi = mysqli_query($koneksi, "DELETE FROM transaksi WHERE id_transaksi = '$id'");
+        $deleteItem = mysqli_query($koneksi, "DELETE FROM transaksi_produk WHERE id_transaksi = '$id'");
+        if (!$deleteTransaksi && !$deleteItem) {
+                die("Gagal menghapus data: " . mysqli_errno($koneksi) .
+                    " - " . mysqli_error($koneksi));
+            } else {
+                echo "<script>alert('Data berhasil dihapus.');window.location='../barangKeluar.php';</script>";
+            }
     } else {
-        echo "<script>alert('Data berhasil dihapus.');window.location='../barangKeluar.php';</script>";
+        die("Gagal memindahkan data: " . mysqli_errno($koneksi) .
+            " - " . mysqli_error($koneksi));
     }
+}
 ?>                              
